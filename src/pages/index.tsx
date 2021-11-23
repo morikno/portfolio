62ed9ec34aafbbe541de586d8ae6b739a7614823
森下光太 <xkotaxmorishitax@gmail.com>
2021-11-23 15:45:17 +0900

    [refactor]cms__init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,38 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "components/layout";
 import utilStyles from "styles/utility/utils.module.scss";
-import { getSortedPostsData } from "lib/posts";
 import Link from "next/link";
-import Date from "components/date";
-import { GetStaticProps } from "next";
+import { client } from "../../libs/client";
 
-
-export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-};
-
-export default function Home({
-  allPostsData,
-}: {
-  allPostsData: {
-    date: string;
-    title: string;
-    id: string;
-  }[];
-}) {
+export default function Home({ blog }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p id="root">モリコノ</p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
+      <section>
+        <h2>Blog</h2>
+        <ul>
+        {blog.map((blog) => (
+          <li key={blog.id}>
+            <Link href={`/blog/${blog.id}`}>
+              <a>{blog.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
       </section>
     </Layout>
   );
 }
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
+
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
